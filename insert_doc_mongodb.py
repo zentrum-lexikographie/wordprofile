7d9978834999a55f7d4606c07c904699cec583fd
rknaebel <rknaebel@uni-potsdam.de>
2020-03-30 13:42:34 +0200

    add multiprocessing db queue, process multiple collections

--- insert_doc_mongodb.py
+++ insert_doc_mongodb.py
@@ -30,8 +30,8 @@ def convert_sentence(sentence: List[Dict[str, str]]) -> List[DBToken]:
     ) for i, token in enumerate(sentence)]
 
 
-def process_doc(mongo_db_keys, doc_id):
-    mongo_db = pymongo.MongoClient(mongo_db_keys[0])[mongo_db_keys[1]][mongo_db_keys[2]]
+def process_doc(mongo_db_keys, mongo_index, doc_id):
+    mongo_db = pymongo.MongoClient(mongo_db_keys[0])[mongo_db_keys[1]][mongo_index]
     doc = mongo_db.find_one({'_id': doc_id})
     try:
         doc_id, db_corpus_file = prepare_corpus_file(doc)
@@ -75,8 +75,8 @@ def create_indices(db_engine_key):
                    "ON matches (relation_label, head_lemma, dep_lemma, head_tag, dep_tag);")
 
 
-def get_corpus_file_ids(mongo_db_keys, db_engine_key, filter_existing=True):
-    mongo_db = pymongo.MongoClient(mongo_db_keys[0])[mongo_db_keys[1]][mongo_db_keys[2]]
+def get_corpus_file_ids(mongo_db_keys, mongo_index, db_engine_key, filter_existing=True):
+    mongo_db = pymongo.MongoClient(mongo_db_keys[0])[mongo_db_keys[1]][mongo_index]
     engine = create_engine(db_engine_key)
     if filter_existing:
         print("LOAD corpus file ids: ", end='')
@@ -91,27 +91,66 @@ def get_corpus_file_ids(mongo_db_keys, db_engine_key, filter_existing=True):
     return document_ids
 
 
-def process_files(mongo_db_keys, db_engine_key):
+def listener(q, db_engine_key):
     engine = create_engine(db_engine_key)
-    document_ids = get_corpus_file_ids(mongo_db_keys, db_engine_key)
-    delete_indices(db_engine_key)
-
-    for doc_i, doc_ids in enumerate(chunks(document_ids, 5000)):
-        with multiprocessing.Pool(10) as pool:
-            db_corpus_files, db_concord_sentences, db_matches = zip(
-                *pool.starmap(process_doc, [(mongo_db_keys, doc_id) for doc_id in doc_ids], chunksize=50)
-            )
-        db_corpus_files = [x._asdict() for x in db_corpus_files if x]
-        db_concord_sentences = [sentence._asdict() for sentences in db_concord_sentences for sentence in sentences]
-        db_matches = [match._asdict() for file_matches in db_matches for match in file_matches]
-        print(doc_i, len(db_corpus_files), len(db_concord_sentences), len(db_matches))
-
+    print('INIT queue, wait for jobs')
+    while True:
+        m = q.get()
+        if not m:
+            print('CLOSE queue')
+            break
         try:
+            db_corpus_files, db_concord_sentences, db_matches = m
+            db_corpus_files = [x._asdict() for x in db_corpus_files if x]
+            db_concord_sentences = [sentence._asdict() for sentences in db_concord_sentences for sentence in sentences]
+            db_matches = [match._asdict() for file_matches in db_matches for match in file_matches]
+
             insert_bulk_corpus_file(engine, db_corpus_files)
             insert_bulk_concord_sentences(engine, db_concord_sentences)
             insert_bulk_matches(engine, db_matches)
+            print('INSERT', len(db_corpus_files), len(db_concord_sentences), len(db_matches))
         except Exception as e:
             print(e)
+
+
+def process_files_index(mongo_db_keys, mongo_index, db_engine_key, db_queue):
+    document_ids = get_corpus_file_ids(mongo_db_keys, mongo_index, db_engine_key, filter_existing=False)
+    for doc_i, doc_ids in enumerate(chunks(document_ids, 3000)):
+        with multiprocessing.Pool(5) as pool:
+            db_corpus_files, db_concord_sentences, db_matches = list(zip(
+                *pool.starmap(process_doc, [(mongo_db_keys, mongo_index, doc_id) for doc_id in doc_ids], chunksize=100)
+            ))
+        db_queue.put((db_corpus_files, db_concord_sentences, db_matches))
+
+
+def process_files(mongo_db_keys, db_engine_key, mongo_indices):
+    mongo_indices = [i.strip() for i in mongo_indices.split(",") if i.strip()]
+    delete_indices(db_engine_key)
+    manager = multiprocessing.Manager()
+    db_queue = manager.Queue()
+    queue_p1 = multiprocessing.Process(target=listener, args=(db_queue, db_engine_key))
+    queue_p2 = multiprocessing.Process(target=listener, args=(db_queue, db_engine_key))
+    queue_p1.start()
+    queue_p2.start()
+    doc_ps = []
+    for mongo_index in mongo_indices:
+        print('Start Process:', mongo_index)
+        p = multiprocessing.Process(target=process_files_index,
+                                    args=(mongo_db_keys, mongo_index, db_engine_key, db_queue))
+        p.start()
+        doc_ps.append((mongo_index, p))
+
+    for mongo_index, p in doc_ps:
+        p.join()
+        print('INDEX DONE:', mongo_index)
+
+    print('INDICES DONE')
+    db_queue.put(None)
+    db_queue.put(None)
+    print('PUT NONE to queue and wait...')
+    queue_p1.join()
+    queue_p2.join()
+    print('ALL DONE')
 
 
 def main():
@@ -134,8 +173,8 @@ def main():
         db_password = args.user
     db_engine_key = 'mysql+pymysql://{}:{}@localhost/{}'.format(args.user, db_password, args.maria_db)
     if args.mongo_index:
-        mongo_db_keys = ("mongodb://localhost:27017/", args.mongo_db, args.mongo_index)
-        process_files(mongo_db_keys, db_engine_key)
+        mongo_db_keys = ("mongodb://localhost:27017/", args.mongo_db)
+        process_files(mongo_db_keys, db_engine_key, args.mongo_index)
     if args.create_index:
         delete_indices(db_engine_key)
         create_indices(db_engine_key)
