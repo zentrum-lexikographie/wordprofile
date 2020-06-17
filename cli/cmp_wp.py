35eef14e23f821f195e81d1413eef4f42c8a594c
rknaebel <rknaebel@uni-potsdam.de>
2020-06-17 14:36:59 +0200

    update wp comparison and add results

--- cli/cmp_wp.py
+++ cli/cmp_wp.py
@@ -9,6 +9,7 @@ from argparse import ArgumentParser
 from collections import defaultdict
 
 import numpy as np
+from tabulate import tabulate
 
 from apps.xmlrpc_api import WordprofileXMLRPC
 
@@ -38,18 +39,24 @@ def get_relation(wp, lemma, pos_tag, start, number, order, min_freq, min_stat, c
     table_items = defaultdict(list)
     for rel_ctr, relation in enumerate(relations):
         for coocc_ctr, coocc in enumerate(relation['Tuples']):
+            if coocc['POS'] in ["Personalpronomen"]:
+                continue
+            if relation["Relation"] == "VZ" and not coocc['Lemma'].endswith(lemma):
+                coocc["Lemma"] = coocc['Lemma'] + lemma
             table_items[relation['Relation']].append((
-                coocc['POS'], coocc["Lemma"], coocc['Score'][order]
+                coocc['POS'], coocc["Lemma"], round(coocc['Score'][order], 2)
             ))
     return dict(table_items)
 
 
-def wp_jaccard_distance(wp1, wp2):
+def wp_jaccard_distance(wp1, wp2, n=10):
     words1 = set(c[1] for c in wp1)
     words2 = set(c[1] for c in wp2)
     overlap = len(words1 & words2)
     both = len(words1 | words2)
-    return overlap, both, round(overlap * 100 / len(words1), 2), round(overlap * 100 / both, 2)
+    overlap_p = round(min(overlap * 100 / min(len(words1), n), 100), 2)
+    jacc = round(min(overlap * 100 / min(both, n * 2), 100), 2)
+    return overlap, both, overlap_p, jacc
 
 
 parser = ArgumentParser()
@@ -94,24 +101,50 @@ pos_map_inv = {v: k for k, v in pos_map.items()}
 
 lemma_pos_candidates = list(map(lambda c: (c[0], pos_map_inv[c[1]]),
                                 (line.strip().split(",") for line in open("cli/token_freqs.csv", "r").readlines())))
-lemma_pos_candidates = random.choices(lemma_pos_candidates, k=100)
+lemma_pos_candidates = random.choices(lemma_pos_candidates, k=200)
 
-scores = {
-    'overlaps': [],
-    'jacc_dists': []
-}
+relations = ['ADV', 'ATTR', 'GMOD', '~GMOD', 'KOM', 'KON', 'OBJ', '~OBJ', 'PP', 'PRED', '~PRED', 'VZ']
+# relations = ['VZ']
 
-for lemma, pos in lemma_pos_candidates:
-    relation_cats_1 = get_relation(wp1, lemma, pos, args.start, args.number, args.order,
-                                   args.min_freq, args.min_stat, args.corpus, args.relations)
-    relation_cats_2 = get_relation(wp2, lemma, pos_map.get(pos), args.start, args.number, args.order,
-                                   args.min_freq, args.min_stat, args.corpus, args.relations)
+for rel in relations:
+    print("Relation:", rel)
+    scores = {
+        'overlaps': [],
+        'jacc_dists': []
+    }
+    with open('eval/wp_{}_cmp.txt'.format(rel.lower().replace("~", "-")), 'w') as fh:
+        for lemma, pos in lemma_pos_candidates:
+            relation_cats_1 = get_relation(wp1, lemma, pos, args.start, args.number, args.order,
+                                           args.min_freq, args.min_stat, args.corpus, [rel])
+            relation_cats_2 = get_relation(wp2, lemma, pos_map.get(pos), args.start, args.number, args.order,
+                                           args.min_freq, args.min_stat, args.corpus, [rel])
 
-    for rel_cat in set(relation_cats_1.keys()) & set(relation_cats_2.keys()):
-        wps1 = relation_cats_1.get(rel_cat, [])
-        wps2 = relation_cats_2.get(rel_cat, [])
-        overlap, both, overlap_prob, jacc_dist = wp_jaccard_distance(wps1, wps2)
-        scores['overlaps'].append(overlap_prob)
-        scores['jacc_dists'].append(jacc_dist)
-        print(lemma, pos, rel_cat, wp_jaccard_distance(wps1, wps2))
-print("Avg Overlap: {}, Avg JACC Dist: {}".format(np.mean(scores['overlaps']), np.mean(scores['jacc_dists'])))
+            for rel_cat in set(relation_cats_1.keys()) & set(relation_cats_2.keys()):
+                wps1 = relation_cats_1.get(rel_cat, [])
+                wps2 = relation_cats_2.get(rel_cat, [])
+                max_rel_no = max(len(wps1), len(wps2))
+                table_items = []
+                table_headers = ['POS (alt)', 'Lemma (alt)', 'Score (alt)', 'POS (neu)', 'Lemma (neu)', 'Score (neu)']
+                for i in range(max_rel_no):
+                    row = []
+                    if i < len(wps1):
+                        row.extend(wps1[i])
+                    else:
+                        row.extend([None, None, None])
+                    if i < len(wps2):
+                        row.extend(wps2[i])
+                    else:
+                        row.extend([None, None, None])
+                    table_items.append(row)
+                overlap, both, overlap_prob, jacc_dist = wp_jaccard_distance(wps1, wps2)
+                scores['overlaps'].append(overlap_prob)
+                scores['jacc_dists'].append(jacc_dist)
+                print("  {}-{} both:{:5.2f} jacc:{:5.2f}".format(lemma, pos, overlap_prob, jacc_dist))
+                print("{}-{} both:{:5.2f} jacc:{:5.2f}".format(lemma, pos, overlap_prob, jacc_dist), file=fh)
+                print(tabulate(table_items, headers=table_headers, tablefmt='fancy_grid'), file=fh)
+                print('', file=fh)
+        print("Avg Overlap: {:5.2f}, Avg JACC Dist: {:5.2f}".format(np.mean(scores['overlaps']),
+                                                                    np.mean(scores['jacc_dists'])))
+        print("Avg Overlap: {:5.2f}, Avg JACC Dist: {:5.2f}".format(np.mean(scores['overlaps']),
+                                                                    np.mean(scores['jacc_dists'])),
+              file=fh)
