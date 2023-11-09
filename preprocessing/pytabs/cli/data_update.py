a06cf9ec675d306fa0e742861d8a2adae6b1a86f
Luise Koehler <luise.koehler@bbaw.de>
2023-11-09 16:59:13 +0100

    Putting it all together in main function

--- preprocessing/pytabs/cli/data_update.py
+++ preprocessing/pytabs/cli/data_update.py
@@ -1,6 +1,11 @@
 import glob
+import os
 import re
+import sys
+from datetime import date
 from typing import Dict, Set
+
+from preprocessing.pytabs.tabs import TabsDocument
 
 
 def collect_current_basenames(data_root: str, corpus_name: str) -> Set[str]:
@@ -37,3 +42,39 @@ def filter_new_files(old_basenames: Set[str], file_basename_mapping: Dict[str, s
         for file, basename in file_basename_mapping.items()
         if basename not in old_basenames
     }
+
+
+def main(
+    corpus: str,
+    data_root: str,
+    ddc_dump_path: str,
+    remove_xml: bool = False,
+    remove_invalid_sentences: bool = False,
+):
+    old_basenames = collect_current_basenames(data_root, corpus)
+    corpus_tabs_file = os.path.join(ddc_dump_path, "corpus-tabs.files")
+    new_file_basename_map = map_tabs_file_to_basename(corpus_tabs_file)
+    files_to_process = filter_new_files(old_basenames, new_file_basename_map)
+
+    output_dir = date.today().isoformat()
+    output_path = os.path.join(data_root, output_dir)
+    os.makedirs(os.path.abspath(output_path), exist_ok=True)
+
+    new_basenames = []
+    with open(os.path.join(output_path, f"{corpus}.conll"), "w") as fp:
+        for file in files_to_process:
+            doc = TabsDocument.from_tabs(os.path.join(ddc_dump_path, file))
+            if remove_xml:
+                doc.remove_xml_tags_from_tabs()
+            if remove_invalid_sentences:
+                doc.remove_invalid_sentence()
+            fp.write(doc.as_conllu())
+            new_basenames.append(doc.meta["basename"])
+
+    with open(os.path.join(output_path, f"{corpus}.toc"), "w", encoding="utf-8") as fp:
+        fp.write("\n".join(new_basenames))
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    main(*args)
