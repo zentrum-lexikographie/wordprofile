6ce035c82de8d080135652d9a21874617dee1d94
Luise Köhler <luise.koehler@bbaw.de>
2024-02-08 10:51:53 +0100

    Format code

--- wordprofile/extract.py
+++ wordprofile/extract.py
@@ -4,86 +4,82 @@ from typing import List, Iterator
 from wordprofile.datatypes import DBToken, Match, DependencyTree
 
 RELATION_PATTERNS = {
-    'ADV': {
-        'desc': "hat Adverbialbestimmung",
-        'inverse': "ist Adverbialbestimmung von",
-        'rules': [
-            ('advmod', 'verb', 'adv'),
-            ('advmod', 'adj', 'adv'),
+    "ADV": {
+        "desc": "hat Adverbialbestimmung",
+        "inverse": "ist Adverbialbestimmung von",
+        "rules": [
+            ("advmod", "verb", "adv"),
+            ("advmod", "adj", "adv"),
         ],
     },
-    'ATTR': {
-        'desc': "hat Adjektivattribut",
-        'inverse': "ist Adjektivattribut von",
-        'rules': [
-            ('amod', 'noun', 'adj'),
+    "ATTR": {
+        "desc": "hat Adjektivattribut",
+        "inverse": "ist Adjektivattribut von",
+        "rules": [
+            ("amod", "noun", "adj"),
         ],
     },
-    'GMOD': {
-        'desc': "hat Genitivattribut",
-        'inverse': "ist Genitivattribut von",
-        'rules': [
+    "GMOD": {
+        "desc": "hat Genitivattribut",
+        "inverse": "ist Genitivattribut von",
+        "rules": [],
+    },
+    "KOM": {
+        "desc": "hat vergleichende Wortgruppe",
+        "inverse": "ist in vergleichender Wortgruppe",
+        "rules": [],
+    },
+    "KON": {
+        "desc": "in Koordination mit",
+        "inverse": "",
+        "rules": [
+            ("conj", "cc", "noun", "noun", "cconj"),
+            ("conj", "cc", "verb", "verb", "cconj"),
+            ("conj", "cc", "adj", "adj", "cconj"),
         ],
     },
-    'KOM': {
-        'desc': "hat vergleichende Wortgruppe",
-        'inverse': "ist in vergleichender Wortgruppe",
-        'rules': [
+    "OBJ": {
+        "desc": "hat Akkusativ/Dativ-Objekt",
+        "inverse": "ist Akkusativ/Dativ-Objekt von",
+        "rules": [
+            ("obj", "verb", "noun"),
+            ("iobj", "verb", "noun"),
         ],
     },
-    'KON': {
-        'desc': "in Koordination mit",
-        'inverse': "",
-        'rules': [
-            ('conj', 'cc', 'noun', 'noun', 'cconj'),
-            ('conj', 'cc', 'verb', 'verb', 'cconj'),
-            ('conj', 'cc', 'adj', 'adj', 'cconj'),
+    "PP": {
+        "desc": "hat Präpositionalgruppe",
+        "inverse": "ist in Präpositionalgruppe",
+        "rules": [
+            ("nmod", "case", "noun", "noun", "adp"),
+            ("obl", "case", "verb", "noun", "adp"),
+            ("obl", "case", "verb", "adj", "adp"),
+            ("obl", "case", "verb", "adv", "adp"),
         ],
     },
-    'OBJ': {
-        'desc': "hat Akkusativ/Dativ-Objekt",
-        'inverse': "ist Akkusativ/Dativ-Objekt von",
-        'rules': [
-            ('obj', 'verb', 'noun'),
-            ('iobj', 'verb', 'noun'),
+    "PRED": {
+        "desc": "hat Prädikativ",
+        "inverse": "ist Prädikativ von",
+        "rules": [],
+    },
+    "SUBJA": {
+        "desc": "hat Subjekt",
+        "inverse": "ist Subjekt von",
+        "rules": [],
+    },
+    "SUBJP": {
+        "desc": "hat Passivsubjekt",
+        "inverse": "ist Passivsubjekt von",
+        "rules": [
+            ("nsubj:pass", "verb", "noun"),
         ],
     },
-    'PP': {
-        'desc': "hat Präpositionalgruppe",
-        'inverse': "ist in Präpositionalgruppe",
-        'rules': [
-            ('nmod', 'case', 'noun', 'noun', 'adp'),
-            ('obl', 'case', 'verb', 'noun', 'adp'),
-            ('obl', 'case', 'verb', 'adj', 'adp'),
-            ('obl', 'case', 'verb', 'adv', 'adp'),
-        ],
-    },
-    'PRED': {
-        'desc': "hat Prädikativ",
-        'inverse': "ist Prädikativ von",
-        'rules': [
-        ],
-    },
-    'SUBJA': {
-        'desc': "hat Subjekt",
-        'inverse': "ist Subjekt von",
-        'rules': [
-        ],
-    },
-    'SUBJP': {
-        'desc': "hat Passivsubjekt",
-        'inverse': "ist Passivsubjekt von",
-        'rules': [
-            ('nsubj:pass', 'verb', 'noun'),
-        ],
-    },
-    'VZ': {
-        'desc': "hat Verbzusatz",
-        'inverse': "",
-        'rules': [
-            ('compound:prt', 'verb', 'adp'),  # liegt ... zugrunde
-            ('compound:prt', 'aux', 'adp'),  # hat ... vor
-            ('compound:prt', 'adj', 'adp'),  # leid tun
+    "VZ": {
+        "desc": "hat Verbzusatz",
+        "inverse": "",
+        "rules": [
+            ("compound:prt", "verb", "adp"),  # liegt ... zugrunde
+            ("compound:prt", "aux", "adp"),  # hat ... vor
+            ("compound:prt", "adj", "adp"),  # leid tun
         ],
     },
 }
@@ -113,8 +109,14 @@ def get_word_classes() -> List[str]:
         else:
             raise ValueError("Unexpected pattern length.")
 
-    return sorted(set(
-        c.upper() for pattern in RELATION_PATTERNS.values() for rule in pattern['rules'] for c in get_classes(rule)))
+    return sorted(
+        set(
+            c.upper()
+            for pattern in RELATION_PATTERNS.values()
+            for rule in pattern["rules"]
+            for c in get_classes(rule)
+        )
+    )
 
 
 def get_inverted_relation_patterns() -> dict:
@@ -125,19 +127,25 @@ def get_inverted_relation_patterns() -> dict:
     """
     relations_inv = defaultdict(lambda: defaultdict(str))
     for relation_dest, relation_patters in RELATION_PATTERNS.items():
-        for p in relation_patters['rules']:
+        for p in relation_patters["rules"]:
             if len(p) == 3:
                 relation_src, head_pos, dep_pos = p
-                relations_inv[relation_src][(head_pos.upper(), dep_pos.upper())] = relation_dest
+                relations_inv[relation_src][
+                    (head_pos.upper(), dep_pos.upper())
+                ] = relation_dest
             elif len(p) == 5:
                 r1, r2, t1, t2, t3 = p
-                relations_inv[(r1, r2)][(t1.upper(), t2.upper(), t3.upper())] = relation_dest
+                relations_inv[(r1, r2)][
+                    (t1.upper(), t2.upper(), t3.upper())
+                ] = relation_dest
             else:
-                raise ValueError('Pattern has unknown dimension')
+                raise ValueError("Pattern has unknown dimension")
     return {k: dict(vd) for k, vd in relations_inv.items()}
 
 
-def extract_matches_by_pattern(relations_inv: dict, tokens: List[DBToken], sid: int) -> Iterator[Match]:
+def extract_matches_by_pattern(
+    relations_inv: dict, tokens: List[DBToken], sid: int
+) -> Iterator[Match]:
     """Extracts matches from a sequence of tokens by using a generated relation dictionary.
 
     Args:
@@ -192,25 +200,34 @@ def extract_comparing_groups(tokens: List[DBToken], sid: int) -> Iterator[Match]
         Generator over extracted matches from sentence.
     """
     for t in tokens:
-        if int(t.head) <= 0 or t.rel != 'case' or t.tag != "CCONJ" or t.surface not in ['als', 'wie']:
+        if (
+            int(t.head) <= 0
+            or t.rel != "case"
+            or t.tag != "CCONJ"
+            or t.surface not in ["als", "wie"]
+        ):
             # token is root
             continue
         t_head_1 = tokens[int(t.head) - 1]
-        if int(t_head_1.head) <= 0 or t_head_1.rel not in {'obl', 'nmod'} or t_head_1.tag != "NOUN":
+        if (
+            int(t_head_1.head) <= 0
+            or t_head_1.rel not in {"obl", "nmod"}
+            or t_head_1.tag != "NOUN"
+        ):
             # token head is root, cannot make ternary relation
             continue
         t_head_2 = tokens[int(t_head_1.head) - 1]
-        if t.surface == 'als':
+        if t.surface == "als":
             # and t_head_2.tag != 'ADJ':
             # expect relations with 'als' to relate to an adjective
             # TODO: preferably check for comparative (https://universaldependencies.org/u/feat/Degree.html)
             continue
-        if t_head_2.tag in {'ADJ', 'VERB', 'NOUN'}:
+        if t_head_2.tag in {"ADJ", "VERB", "NOUN"}:
             yield Match(
                 t_head_2,
                 t_head_1,
                 None,
-                'KOM',
+                "KOM",
                 sid,
             )
 
@@ -228,11 +245,11 @@ def extract_predicatives(dtree: DependencyTree, sid: int) -> Iterator[Match]:
     """
     for n in dtree.nodes:
         # subject predicative
-        if n.token.tag in {'NOUN', 'VERB', 'ADJ'}:
+        if n.token.tag in {"NOUN", "VERB", "ADJ"}:
             if any(c.token.rel == "cop" and c.token.tag == "AUX" for c in n.children):
                 if not any(c.token.rel == "case" for c in n.children):
                     for nsubj in n.children:
-                        if nsubj.token.rel == "nsubj" and nsubj.token.tag == 'NOUN':
+                        if nsubj.token.rel == "nsubj" and nsubj.token.tag == "NOUN":
                             yield Match(
                                 nsubj.token,
                                 n.token,
@@ -241,11 +258,14 @@ def extract_predicatives(dtree: DependencyTree, sid: int) -> Iterator[Match]:
                                 sid,
                             )
         # object predicative
-        if n.token.tag == 'VERB':
+        if n.token.tag == "VERB":
             for obj in n.children:
-                if obj.token.tag in {'VERB', 'ADJ', 'NOUN'} and obj.token.rel in {'obj', 'obl'}:  # ++ 'advcl', 'xcomp'
+                if obj.token.tag in {"VERB", "ADJ", "NOUN"} and obj.token.rel in {
+                    "obj",
+                    "obl",
+                }:  # ++ 'advcl', 'xcomp'
                     # if any(c.token.rel in {'mark', 'case'} and c.token.tag in {'CCONJ', 'ADP'} for c in obj.children):
-                    if any(c.token.surface in {'als', 'für'} for c in obj.children):
+                    if any(c.token.surface in {"als", "für"} for c in obj.children):
                         yield Match(
                             n.token,
                             obj.token,
@@ -266,89 +286,89 @@ def extract_genitives(dtree: DependencyTree, sid: int) -> Iterator[Match]:
         Generator over extracted matches from sentence.
     """
     determiners = {
-            'aller' ,
-            'alles' ,
-            'beider' ,
-            'Deinen' ,
-            'deiner' ,
-            'Deiner' ,
-            'deines' ,
-            'Deines' ,
-            'der' ,
-            'des' ,
-            'dieser' ,
-            'dieses' ,
-            'dreier' ,
-            'ebender' ,
-            'ebendes' ,
-            'ebendieser' ,
-            'ebendieses' ,
-            'ebenjener' ,
-            'ebenjenes' ,
-            'ebensolcher' ,
-            'ebensolches' ,
-            'einer' ,
-            'eines' ,
-            'einiger' ,
-            'einiges' ,
-            'etlicher' ,
-            'etliches' ,
-            'euerer' ,
-            'Euerer' ,
-            'eueres' ,
-            'Eueres' ,
-            'euers' ,
-            'Euers' ,
-            'eurer' ,
-            'Eurer' ,
-            'eures' ,
-            'Eures' ,
-            'ihrer' ,
-            'Ihrer' ,
-            'ihres' ,
-            'Ihres' ,
-            'irgendeiner' ,
-            'irgendeines' ,
-            'irgendwelcher' ,
-            'irgendwelches' ,
-            'jeder' ,
-            'jedes' ,
-            'jedweder' ,
-            'jedwedes' ,
-            'jeglicher' ,
-            'jegliches' ,
-            'jener' ,
-            'jenes' ,
-            'keiner' ,
-            'keines' ,
-            'mancher' ,
-            'manches' ,
-            'mehrerer' ,
-            'meiner' ,
-            'meines' ,
-            '’ner' ,
-            '’nes' ,
-            'sämtlicher' ,
-            'sämtliches' ,
-            'seiner' ,
-            'seines' ,
-            'solcher' ,
-            'solches' ,
-            'unserer' ,
-            'unseres' ,
-            'unsers' ,
-            'unsrer' ,
-            'unsres' ,
-            'welcher' ,
-            'welches' ,
-            'zweier' ,
-        }
+        "aller",
+        "alles",
+        "beider",
+        "Deinen",
+        "deiner",
+        "Deiner",
+        "deines",
+        "Deines",
+        "der",
+        "des",
+        "dieser",
+        "dieses",
+        "dreier",
+        "ebender",
+        "ebendes",
+        "ebendieser",
+        "ebendieses",
+        "ebenjener",
+        "ebenjenes",
+        "ebensolcher",
+        "ebensolches",
+        "einer",
+        "eines",
+        "einiger",
+        "einiges",
+        "etlicher",
+        "etliches",
+        "euerer",
+        "Euerer",
+        "eueres",
+        "Eueres",
+        "euers",
+        "Euers",
+        "eurer",
+        "Eurer",
+        "eures",
+        "Eures",
+        "ihrer",
+        "Ihrer",
+        "ihres",
+        "Ihres",
+        "irgendeiner",
+        "irgendeines",
+        "irgendwelcher",
+        "irgendwelches",
+        "jeder",
+        "jedes",
+        "jedweder",
+        "jedwedes",
+        "jeglicher",
+        "jegliches",
+        "jener",
+        "jenes",
+        "keiner",
+        "keines",
+        "mancher",
+        "manches",
+        "mehrerer",
+        "meiner",
+        "meines",
+        "’ner",
+        "’nes",
+        "sämtlicher",
+        "sämtliches",
+        "seiner",
+        "seines",
+        "solcher",
+        "solches",
+        "unserer",
+        "unseres",
+        "unsers",
+        "unsrer",
+        "unsres",
+        "welcher",
+        "welches",
+        "zweier",
+    }
     for n in dtree.nodes:
-        if n.token.tag == 'NOUN':
+        if n.token.tag == "NOUN":
             for nmod in n.children:
-                if nmod.token.rel == "nmod" and nmod.token.tag == 'NOUN':
+                if nmod.token.rel == "nmod" and nmod.token.tag == "NOUN":
                     if any(det.token.surface in determiners for det in nmod.children):
-                        if any(c.token.rel == 'case' for c in nmod.children):
+                        if any(c.token.rel == "case" for c in nmod.children):
                             continue
                         yield Match(
                             n.token,
@@ -372,9 +392,9 @@ def extract_active_subjects(dtree: DependencyTree, sid: int) -> Iterator[Match]:
     for n in dtree.nodes:
         if any(cop.token.rel == "cop" for cop in n.children):
             continue
-        if n.token.tag in {'NOUN', 'VERB', 'ADJ'}:
+        if n.token.tag in {"NOUN", "VERB", "ADJ"}:
             for nsubj in n.children:
-                if nsubj.token.rel == "nsubj" and nsubj.token.tag == 'NOUN':
+                if nsubj.token.rel == "nsubj" and nsubj.token.tag == "NOUN":
                     yield Match(
                         n.token,
                         nsubj.token,
