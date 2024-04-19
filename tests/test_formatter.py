e55dc1d7748bb525c498821c9e0edacf3f42f675
Luise Köhler <luise.koehler@bbaw.de>
2024-04-19 17:10:53 +0200

    Test highlighting of concordances

---
+++ tests/test_formatter.py
@@ -0,0 +1,56 @@
+import datetime
+import wordprofile.formatter as form
+from wordprofile.datatypes import Concordance, MweConcordance
+
+
+def test_highlighting_of_single_word_concordance():
+    sentence = Concordance(
+        sentence="Man\x02könne\x02Fahrgäste\x02schließlich\x02nicht\x02zwingen\x01,\x02sich\x02zu\x02waschen\x01.",
+        token_position_1=3,
+        token_position_2=6,
+        prep_position=5,
+        corpus="corpus",
+        date=datetime.date.fromisoformat("2024-01-01"),
+        textclass="tc",
+        orig="",
+        avail="",
+        page="",
+        file="",
+        scan="",
+        score=5,
+        sentence_left="",
+        sentence_right="",
+    )
+    result = form.format_concordances([sentence])[0]
+    assert (
+        result["ConcordLine"]
+        == "Man könne &&Fahrgäste&& schließlich _&nicht&_ _&zwingen&_, sich zu waschen."
+    )
+
+
+def test_highlighting_of_mwe_concordance():
+    sentence = MweConcordance(
+        sentence="Man\x02könne\x02Fahrgäste\x02schließlich\x02nicht\x02zwingen\x01,\x02sich\x02zu\x02waschen\x01.",
+        token1_position_1=3,
+        token1_position_2=6,
+        prep1_position=4,
+        token2_position_1=9,
+        token2_position_2=8,
+        prep2_position=0,
+        corpus="corpus",
+        date=datetime.date.fromisoformat("2024-01-01"),
+        textclass="tc",
+        orig="",
+        avail="",
+        page="",
+        file="",
+        scan="",
+        score=5,
+        sentence_left="",
+        sentence_right="",
+    )
+    result = form.format_mwe_concordances([sentence])[0]
+    assert (
+        result["ConcordLine"]
+        == "Man könne _&Fahrgäste&_ &&schließlich&& nicht &&zwingen&&, &&sich&& &&zu&& waschen."
+    )
