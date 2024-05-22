78fe0fcc1054351458529c08c483b0e94be3f059
Luise Koehler <luise.koehler@bbaw.de>
2024-05-22 13:52:27 +0200

    Add tests for parsing and annotation

--- preprocessing/tests/test_annotate_deprel.py
+++ preprocessing/tests/test_annotate_deprel.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
+import conllu
 import pytest
+import spacy
 
 import preprocessing.annotate_deprel as deprel
 
@@ -15,3 +17,55 @@ def short_conll_file():
 @pytest.fixture
 def multiple_docs_conll_file():
     return TEST_DIR / "testdata" / "four_docs.conll"
+
+
+@pytest.fixture(scope="module")
+def parser():
+    # determine which model to use
+    try:
+        import de_dwds_dep_hdt_lg
+
+        model = "de_dwds_dep_hdt_lg"
+    except ImportError:
+        import de_dwds_dep_hdt_dist
+
+        model = "de_dwds_dep_hdt_dist"
+    return deprel.SpacyParser(model)
+
+
+def test_custom_tokenizer(parser, short_conll_file):
+    with open(short_conll_file) as fh:
+        sentences = list(conllu.parse_incr(fh))
+    sentence = sentences[0]
+    sentence[1]["form"] = ""
+    result = parser.custom_tokenizer(sentence)
+    assert result[0][1].text == "---"
+
+
+def test_parser_returns_annotations_and_original(parser, multiple_docs_conll_file):
+    with open(multiple_docs_conll_file) as fh:
+        doc = list(conllu.parse_incr(fh))
+        result = next(parser(doc))
+    assert len(result) == 2
+    assert isinstance(result[0], spacy.tokens.Doc)
+    assert isinstance(result[1], conllu.models.TokenList)
+    assert result[1] == doc[0]
+
+
+def test_add_token_annotation_to_conll_sentence(short_conll_file, parser):
+    with open(short_conll_file) as fh:
+        sentences = conllu.parse(fh.read())
+    doc, conll_sent = next(parser(sentences[3:4]))
+    deprel.add_annotation_to_tokens(conll_sent, doc)
+    assert [
+        (tok["form"], tok["upos"], tok["head"], tok["deprel"]) for tok in conll_sent
+    ] == [
+        ("Damals", "ADV", 2, "advmod"),
+        ("ging", "VERB", 0, "ROOT"),
+        ("eine", "DET", 5, "det"),
+        ("ganze", "ADJ", 5, "amod"),
+        ("Epoche", "NOUN", 2, "nsubj"),
+        ("zu", "ADP", 7, "case"),
+        ("Ende", "NOUN", 2, "obl"),
+        (".", "PUNCT", 2, "punct"),
+    ]
