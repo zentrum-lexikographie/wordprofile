662ce53227831c44b3bca4c746a5d23010c4c17c
Luise Koehler <luise.koehler@bbaw.de>
2024-06-18 16:22:05 +0200

    Format sql query, udpate docstring

--- wordprofile/wpse/mwe_connector.py
+++ wordprofile/wpse/mwe_connector.py
@@ -166,7 +166,7 @@ class WPMweConnect:
     def get_relation_tuples(
         self, coocc_ids: List[int], order_by: str, min_freq: int, min_stat: float
     ) -> List[Coocc]:
-        """Fetches MWE with related statistics for a specific relation from database backend.
+        """Fetches MWE with related statistics for collocation ids from database.
 
         Args:
             coocc_ids: List of Collocation ids, one per relation.
@@ -178,8 +178,9 @@ class WPMweConnect:
         """
         sql = """
         SELECT
-            mwe.id, mwe.label, mwe.lemma, mwe.lemma_tag, mwe.frequency, c.lemma1, c.lemma2, c.lemma1_tag, c.lemma2_tag,
-            IFNULL(mwe.score, 0.0) as log_dice, tf_mwe.surface, tf1.surface, tf2.surface, mwe.inv,
+            mwe.id, mwe.label, mwe.lemma, mwe.lemma_tag, mwe.frequency, c.lemma1,
+            c.lemma2, c.lemma1_tag, c.lemma2_tag, IFNULL(mwe.score, 0.0) as log_dice,
+            tf_mwe.surface, tf1.surface, tf2.surface, mwe.inv,
             (SELECT COUNT(*) FROM mwe_match m WHERE m.mwe_id = ABS(mwe.id)) as num_concords
         FROM mwe
         JOIN collocations as c ON (mwe.collocation1_id = c.id)
