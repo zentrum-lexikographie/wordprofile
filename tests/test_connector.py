ca97f0a1d9ed9ffa6a33955d11c6085f2b079285
Luise Koehler <luise.koehler@bbaw.de>
2025-03-04 15:50:59 +0100

    fix: handle invalid ids for MWE
    Log error instead of raising exception for invalid ids and return none.
    The check if the result set contains more than one element and subsequent raising of InternalError has been removed since the unique index on mwe.id should make this case impossible.

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -813,3 +813,11 @@ class WPMweConnectTest(unittest.TestCase):
             prep="vor",
         )
         self.assertEqual(result[0], expected)
+
+    def test_invalid_id_returns_none(self):
+        self.assertIsNone(self.mwe_connector.get_relation_by_id(42))
+
+    def test_logging_for_invalid_id(self):
+        with self.assertLogs() as logged:
+            self.mwe_connector.get_relation_by_id(42)
+            assert "Invalid Id: 42" in logged.output[0]
